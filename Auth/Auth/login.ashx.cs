d1cee159b47e4f1d618582f45caca94226c4ebf5
gaoz <gaoz@ntchst.com>
2019-10-16 17:00:24 +0800

    项目初始化

---
+++ Auth/Auth/login.ashx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Auth
+{
+    /// <summary>
+    /// login 的摘要说明
+    /// </summary>
+    public class login : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+
+            string lUserName = context.Request.Form["txtUserName"].ToString();
+            string lPassword = context.Request.Form["txtPassword"].ToString();
+            Login lLogin = new Login();
+            if (lLogin.Check(lUserName, lPassword))
+            {
+                context.Response.Redirect("home.html");
+            }
+            else
+            {
+                context.Response.Redirect("login.html");
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
