43bf3cb0cc15507e862d23eac55a5cf1790cdc18
nnugaozhiLap <nnugaozhi@hotmail.com>
2020-01-05 00:06:24 +0800

    创建js标签页共通

--- RBACPractice/JSLIB/TabPageController/TabPageController.js
+++ RBACPractice/JSLIB/TabPageController/TabPageController.js
@@ -1,21 +1,159 @@
 ﻿//标签页面管理类
 function CTabPageController() {
+    //标签容器
     this.TabContainer = null;
+    //页面容器
     this.PageContainer = null;
+    //标签页数组
     this.TabPages = new Array();
+    //当前标签页下标
+    this.CurrentTagePageIndex = -1;
+}
+
+//创建Tab栏前后滑动导航按钮
+CTabPageController.prototype.CreateNav = function () {
+    var lBackwordDiv = $('<div>');
+    lBackwordDiv.css('border-right', '1px solid blue');
+    lBackwordDiv.css('float', 'left');
+    lBackwordDiv.css('cursor', 'pointer')
+    lBackwordDiv.css('width', '30px');
+    lBackwordDiv.css('text-align', 'center');
+    lBackwordDiv.on('click', { TabPageController: this }, BackwordDiv_OnClick);
+
+    var lBackwordImg = $('<img>');
+    lBackwordImg.attr('src', '/JSLIB/TabPageController/theme/default/backword.png');
+    lBackwordImg.css('width', '16px');
+    lBackwordImg.css('height', '16px');
+    lBackwordDiv.append(lBackwordImg);
+    this.TabContainer.append(lBackwordDiv);
+
+    var lForwordDiv = $('<div>');
+    lForwordDiv.css('border-left', '1px solid green');
+    lForwordDiv.css('float', 'right');
+    lForwordDiv.css('cursor', 'pointer')
+    lForwordDiv.css('width', '30px');
+    lForwordDiv.css('text-align', 'center');
+    lForwordDiv.on('click', { TabPageController: this }, ForwordDiv_OnClick);
+
+    var lForwordImg = $('<img>');
+    lForwordImg.attr('src', '/JSLIB/TabPageController/theme/default/forword.png');
+    lForwordImg.css('width', '16px');
+    lForwordImg.css('height', '16px');
+    lForwordDiv.append(lForwordImg);
+
+    this.TabContainer.append(lForwordDiv);
+}
+
+//标签栏向后滑动单击事件
+function BackwordDiv_OnClick(event) {
+    var lTabPageController = event.data.TabPageController;
+    var lLastHiddenIndexBegin = -1;
+    var lFirstVisibleIndexEnd = -1;
+    var lId = '';
+    for (var i = 0; i <= lTabPageController.TabPages.length - 1; i++) {
+        lId = lTabPageController.TabPages[i].Id;
+        if (lTabPageController.TabContainer.find('div[id=' + lId + ']').css('display') == 'none') {
+            lLastHiddenIndexBegin = i;
+        } else {
+            break;
+        }
+    }
+
+    if (lLastHiddenIndexBegin != -1) {
+        lId = lTabPageController.TabPages[lLastHiddenIndexBegin].Id;
+        lTabPageController.TabContainer.find('div[id=' + lId + ']').css('display', 'block');
+    } else {
+        return;
+    }
+
+    for (var i = lLastHiddenIndexBegin; i < lTabPageController.TabPages.length; i++) {
+        lId = lTabPageController.TabPages[i].Id;
+        if (lTabPageController.TabContainer.find('div[id=' + lId + ']').css('display') == 'block') {
+            lFirstVisibleIndexEnd = i;
+        } else {
+            break;
+        }
+    }
+
+    if (lFirstVisibleIndexEnd != -1) {
+        lId = lTabPageController.TabPages[lFirstVisibleIndexEnd].Id;
+        lTabPageController.TabContainer.find('div[id=' + lId + ']').css('display', 'none');
+    }
+}
+
+//标签栏向前滑动单击事件
+function ForwordDiv_OnClick(event) {
+    var lTabPageController = event.data.TabPageController;
+    var lFirstHiddenIndexEnd = -1;
+    var lLastVisibleIndexBegin = -1;
+    var lId = '';
+    for (var i = lTabPageController.TabPages.length - 1; i >= 0; i--) {
+        lId = lTabPageController.TabPages[i].Id;
+        if (lTabPageController.TabContainer.find('div[id=' + lId + ']').css('display') == 'none') {
+            lFirstHiddenIndexEnd = i;
+        } else {
+            break;
+        }
+    }
+
+    if (lFirstHiddenIndexEnd != -1) {
+        lId = lTabPageController.TabPages[lFirstHiddenIndexEnd].Id;
+        lTabPageController.TabContainer.find('div[id=' + lId + ']').css('display', 'block');
+    } else {
+        return;
+    }
+
+    for (var i = lFirstHiddenIndexEnd; i >= 0; i--) {
+        lId = lTabPageController.TabPages[i].Id;
+        if (lTabPageController.TabContainer.find('div[id=' + lId + ']').css('display') == 'block') {
+            lLastVisibleIndexBegin = i;
+        } else {
+            break;
+        }
+    }
+
+    if (lLastVisibleIndexBegin != -1) {
+        lId = lTabPageController.TabPages[lLastVisibleIndexBegin].Id;
+        lTabPageController.TabContainer.find('div[id=' + lId + ']').css('display', 'none');
+    }
+
 }
 
 //展示指定标签页
 CTabPageController.prototype.Show = function (paraId, paraName, paraUrl) {
     var isNew = true;
+    var lTotalWidth = this.TabContainer.css('width').replace('px', '');
+    var lOccupiedWidth = 60;
 
     for (var i = 0; i < this.TabPages.length; i++) {
         if (this.TabPages[i].Id == paraId) {
             isNew = false;
-            this.TabContainer.find('div[id=' + paraId + '] span').css('background-color', 'blue');
+            this.TabContainer.find('div[id=' + paraId + '] div').css('background-color', 'blue');
+            this.PageContainer.find('iframe[id=' + paraId + ']').css('display', 'block');
+            if (this.TabContainer.find('div[id=' + paraId + ']').css('display') == 'none') {
+
+                for (var j = i; j < this.TabPages.length; j++) {
+                    lOccupiedWidth += parseInt(this.TabContainer.find('div[id=' + this.TabPages[j].Id + ']').css('width').replace('px', ''));
+                    if (lOccupiedWidth < lTotalWidth) {
+                        this.TabContainer.find('div[id=' + this.TabPages[j].Id + ']').css('display', 'block');
+                    } else {
+                        this.TabContainer.find('div[id=' + this.TabPages[j].Id + ']').css('display', 'none');
+                    }
+                }
+
+                for (var j = i - 1; j >= 0; j--) {
+                    lOccupiedWidth += parseInt(this.TabContainer.find('div[id=' + this.TabPages[j].Id + ']').css('width').replace('px', ''));
+                    if (lOccupiedWidth < lTotalWidth) {
+                        this.TabContainer.find('div[id=' + this.TabPages[j].Id + ']').css('display', 'block');
+                    } else {
+                        this.TabContainer.find('div[id=' + this.TabPages[j].Id + ']').css('display', 'none');
+                    }
+                }
+            }
         }
         else {
-            this.TabContainer.find('div[id=' + this.Tabs[i].TabId + '] span').css('background-color', 'grey');
+            this.TabContainer.find('div[id=' + this.TabPages[i].Id + '] div').css('background-color', 'grey');
+            this.PageContainer.find('iframe[id=' + this.TabPages[i].Id + ']').css('display', 'none');
         }
     }
 
@@ -24,65 +162,117 @@ CTabPageController.prototype.Show = function (paraId, paraName, paraUrl) {
         lTabPage.Id = paraId;
         lTabPage.Addr = paraUrl;
         lTabPage.Name = paraName;
-        lTabPage.Controller = this;
-        this.TabPages.push(lTab);
+        lTabPage.TabPageController = this;
+        lTabPage.Show();
+        this.TabPages.push(lTabPage);
+        this.CurrentTagePageIndex = this.TabPages.length - 1;
+
+        var i = this.CurrentTagePageIndex;
+        for (; i >= 0; i--) {
+            var lId = this.TabPages[i].Id;
+            if (this.TabContainer.find('div[id=' + lId + ']').css('display') == 'block') {
+                lOccupiedWidth += parseInt(this.TabContainer.find('div[id=' + lId + ']').css('width').replace('px', ''));
+            }
+            if (lOccupiedWidth > lTotalWidth) {
+                break;
+            }
+        }
+
+        for (var j = i; j >= 0; j--) {
+            var lId = this.TabPages[j].Id;
+            if (this.TabContainer.find('div[id=' + lId + ']').css('display') == 'block') {
+                this.TabContainer.find('div[id=' + lId + ']').css('display', 'none');
+            } else {
+                break;
+            }
+        }
     }
 }
 
 //标签类
 function CTabPage() {
-    this.TabId = "";
-    this.TabUrl = "";
-    this.TabName = "";
-    this.TabController = null;
+    this.Id = "";
+    this.Addr = "";
+    this.Name = "";
+    this.TabPageController = null;
 }
 
 //创建标签
 CTabPage.prototype.Show = function () {
-    var tabDiv = $('<div style="float:left;padding-right:20px;">');
-    var tabNameSpan = $('<span>');
+
+    //创建标签HTML
+    var tabDiv = $('<div style="float:left;">');
+    var tabNameDiv = $('<div style="float:left;width:120px;">');
     var tabCloseIcon = $('<img src="/JSLIB/TabPageController/theme/default/close.png">');
 
-    tabDiv.attr('id', this.TabId);
+    tabDiv.attr('id', this.Id);
 
-    tabNameSpan.text(this.TabName);
-    tabNameSpan.css('background-color', 'blue');
-    tabNameSpan.css('cursor', 'pointer');
-    tabNameSpan.on('click', { TabObj: this }, this.NameSpan_OnClick);
-
+    tabNameDiv.text(this.Name);
+    tabNameDiv.css('background-color', 'blue');
+    tabNameDiv.css('cursor', 'pointer');
+    tabNameDiv.on('click', { TabObj: this }, this.NameDiv_OnClick);
     tabCloseIcon.on('click', { TabObj: this }, this.CloseIcon_OnClick);
 
-    tabDiv.append(tabNameSpan);
+    tabDiv.append(tabNameDiv);
     tabDiv.append(tabCloseIcon);
 
-    tabDiv;
+    //将标签HTML加入标签容器
+    this.TabPageController.TabContainer.append(tabDiv);
 
+    //创建页面HTML
+    var lFrame = $('<iframe>');
+    lFrame.attr('id', this.Id);
+    lFrame.attr('src', this.Addr);
+    lFrame.attr('frameborder', 'no');
+    lFrame.css('width', '800px');
+    lFrame.css('height', '600px');
+    lFrame.css('background-color', '#654321');
+
+    //将页面HTML加入标签容器
+    this.TabPageController.PageContainer.append(lFrame);
 }
 
 //标签关闭事件
 CTabPage.prototype.CloseIcon_OnClick = function (event) {
     var lTabObj = event.data.TabObj;
-    var lTabController = lTabObj.TabController;
+    var lTabPageController = lTabObj.TabPageController;
 
-    lTabController.TabContainer.find('div[id=' + lTabObj.TabId + ']').remove();
-    for (var i = 0; i < lTabController.Tabs.length; i++) {
-        if (lTabController.Tabs[i].TabId == lTabObj.TabId) {
-            lTabController.Tabs.splice(i, 1);
+    lTabPageController.TabContainer.find('div[id=' + lTabObj.Id + ']').remove();
+    lTabPageController.PageContainer.find('iframe[id=' + lTabObj.Id + ']').remove();
+
+    for (var i = 0; i < lTabPageController.TabPages.length; i++) {
+        if (lTabPageController.TabPages[i].Id == lTabObj.Id) {
+            lTabPageController.TabPages.splice(i, 1);
+            if (lTabPageController.CurrentTagePageIndex == i) {
+                if (i < lTabPageController.TabPages.length) {
+                    //设置后一页为当前页
+                    lTabPageController.TabPages[i].NameDiv_OnClick({ data: { TabObj: lTabPageController.TabPages[i] } });
+                } else if (i - 1 >= 0 && i - 1 < lTabPageController.TabPages.length) {
+                    //设置前一页为当前页
+                    lTabPageController.TabPages[i - 1].NameDiv_OnClick({ data: { TabObj: lTabPageController.TabPages[i - 1] } });
+                    lTabPageController.CurrentTagePageIndex = i - 1;
+                } else {
+                    lTabPageController.CurrentTagePageIndex = -1;
+                }
+            }
         }
     }
 }
 
 //标签单击事件
-CTabPage.prototype.NameSpan_OnClick = function (event) {
+CTabPage.prototype.NameDiv_OnClick = function (event) {
     var lTabObj = event.data.TabObj;
-    var lTabController = lTabObj.TabController;
+    var lTabPageController = lTabObj.TabPageController;
 
-    for (var i = 0; i < lTabController.Tabs.length; i++) {
-        if (lTabController.Tabs[i].TabId == lTabObj.TabId) {
-            lTabController.TabContainer.find('div[id=' + lTabController.Tabs[i].TabId + '] span').css('background-color', 'blue');
+    for (var i = 0; i < lTabPageController.TabPages.length; i++) {
+        if (lTabPageController.TabPages[i].Id == lTabObj.Id) {
+            lTabPageController.TabContainer.find('div[id=' + lTabPageController.TabPages[i].Id + '] div').css('background-color', 'blue');
+            lTabPageController.PageContainer.find('iframe[id=' + lTabPageController.TabPages[i].Id + ']').css('display', 'block');
+            lTabPageController.CurrentTagePageIndex = i;
         }
         else {
-            lTabController.TabContainer.find('div[id=' + lTabController.Tabs[i].TabId + '] span').css('background-color', 'grey');
+            lTabPageController.TabContainer.find('div[id=' + lTabPageController.TabPages[i].Id + '] div').css('background-color', 'grey');
+            lTabPageController.PageContainer.find('iframe[id=' + lTabPageController.TabPages[i].Id + ']').css('display', 'none');
         }
     }
 }
