94099c84e91b583ee6d4fa16ff1ed5f46e17bd98
nnugaozhiLap <nnugaozhi@hotmail.com>
2020-01-07 23:37:07 +0800

    添加登陆页面及用户验证

---
+++ RBACPractice/Handler/Login.ashx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.SessionState;
+
+namespace RBACPractice.Handler
+{
+    /// <summary>
+    /// Login 的摘要说明
+    /// </summary>
+    public class Login : IHttpHandler, IRequiresSessionState
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string lRequestMethod = context.Request.Params["RequestMethod"].ToString();
+
+            switch (lRequestMethod)
+            {
+                case "login":
+                    LoginCheck(context);
+                    break;
+            }
+        }
+
+        private void LoginCheck(HttpContext context)
+        {
+            string lUserName = context.Request.Params["UserName"].ToString();
+            string lPassword = context.Request.Params["Password"].ToString();
+            //context.Session = new HttpSessionState();
+            context.Session.Add("UserID", lUserName);
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("{Status:1,Msg:''}");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
